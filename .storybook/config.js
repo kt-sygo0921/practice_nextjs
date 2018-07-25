113b3048b87c8aef71e33dd6d8313b617ecdd26e
加藤翔吾 <>
2018-07-26 00:15:38 +0900

    storybook修正

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,4 @@
 import { configure} from '@storybook/react';
-import { withNotes } from '@storybook/addon-notes';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../components/', true, /.stories.js$/);
